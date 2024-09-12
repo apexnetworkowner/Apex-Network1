c8ec57e799c78afe23dccb500634d6e21c3229e0
apexnetworkowner <166319415+apexnetworkowner@users.noreply.github.com>
2024-09-12 13:09:24 -0500

    Add files via upload

--- blue.js
+++ blue.js
@@ -1,12 +1,9 @@
-// Get the canvas element
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-// Set the canvas dimensions
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Define the particle class
 class Particle {
     constructor(x, y) {
         this.x = x;
@@ -18,7 +15,7 @@ class Particle {
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+        ctx.fillStyle = 'rgba(0, 0, 255, 0.5)'; // blue particle color
         ctx.fill();
     }
 
@@ -31,13 +28,11 @@ class Particle {
     }
 }
 
-// Create an array of particles
 const particles = [];
 for (let i = 0; i < 100; i++) {
     particles.push(new Particle(Math.random() * canvas.width, Math.random() * canvas.height));
 }
 
-// Animate the particles
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < particles.length; i++) {
